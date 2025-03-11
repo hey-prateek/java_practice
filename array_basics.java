795891ceb322b792aa81fb802b7c99091cd1b72d
prateek <71164915+hey-prateek@users.noreply.github.com>
2025-03-11 18:14:43 +0530

    Add files via upload

---
+++ array_basics.java
@@ -0,0 +1,45 @@
+public class array_basics {
+    public static void main(String[] args) {
+
+        // array basic operations
+        int[] arr = new int[] { 1, 2, 3, 4 };
+        int arr_1[][] = { { 1 }, { 2 } };
+        for (int i : arr)
+            System.out.print(i + " ");
+        System.out.println();
+        for (int i[] : arr_1)
+            System.out.print(i[0] + " ");
+            System.out.println();
+
+        // object array
+        muskan m1=new muskan(20, "pink");
+        muskan m2=new muskan(25, "blue");
+        muskan[] arr_obj={m1, m2};
+        System.out.println(arr_obj[1].age);
+
+        // clone array
+        int[] arr_2={1,2,3};
+        int[] clone_arr=arr_2.clone();
+        System.out.println(arr_2==clone_arr);
+        arr_2[1]=4;
+        System.out.println(arr_2[1]+" "+clone_arr[1]);
+
+
+
+    }
+
+}
+
+class muskan{
+
+    // constructor
+    muskan(int age, String fav_color)
+    {
+        this.age=age;
+        this.fav_color=fav_color;
+    }
+
+    //local variables
+    int age;
+    String fav_color;
+}
