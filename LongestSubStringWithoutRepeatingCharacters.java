7b346cbfed9e8a40b18a9d1c77ad177a3016744b
hey-prateek <prateektiwari010799@gmail.com>
2025-09-14 22:35:00 +0530

    few java practice codes

---
+++ LongestSubStringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+// Given a string s, find the length of the longest substring without
+// repeating characters.
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubStringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        String str="heyprateek";
+
+        System.out.print(longestSubstring(str));
+    }
+    static int longestSubstring(String s)
+    {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+
+    }
+}
