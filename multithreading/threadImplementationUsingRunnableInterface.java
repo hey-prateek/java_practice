c15b9005721049efd9beaf3139cad3b3cb8278f8
hey-prateek <prateektiwari010799@gmail.com>
2025-09-19 19:22:18 +0530

    merging multithreading practice codes

--- multithreading/threadImplementationUsingRunnableInterface.java
+++ multithreading/threadImplementationUsingRunnableInterface.java
@@ -1,15 +1,24 @@
-class myThread implements Runnable
-{
-    public void run()
-    {
-        System.out.println("inside class implementing runnable interface...");
+class MyTask implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            try {
+                Thread.sleep(500); // pause 0.5 sec
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
 public class threadImplementationUsingRunnableInterface {
-    public static void main(String[] args)
-    {
-        Thread thread=new Thread(new myThread());
-        thread.start();
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+
+        Thread t1 = new Thread(task, "Worker-1");
+        Thread t2 = new Thread(task, "Worker-2");
+
+        t1.start();
+        t2.start();
     }
 }
